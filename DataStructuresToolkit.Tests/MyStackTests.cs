f6feb724496243ff23cfa35a8d3adcd73caea419
Brittany Hancock <femaleentp@gmail.com>
2025-10-12 16:57:55 -0700

    Week3: add first MyStack test (Push increases Count, Peek shows top)

---
+++ DataStructuresToolkit.Tests/MyStackTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using DataStructuresToolkit.StacksQueues;
+
+namespace DataStructuresToolkit.Tests
+{
+    public class MyStackTests
+    {
+        [Fact]
+        public void Push_IncreasesCount_AndPeekShowsTop()
+        {
+            // Arrange
+            var stack = new MyStack<int>();
+            // Act
+            stack.Push(10);
+            stack.Push(20);
+            // Assert
+            Assert.Equal(2, stack.Count);
+            Assert.Equal(20, stack.Peek()); 
+        }
+    }
+}
