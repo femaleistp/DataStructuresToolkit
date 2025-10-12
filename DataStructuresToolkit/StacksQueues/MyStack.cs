0e27b45df5be78d3ace61a8769ac3f35ac0c6cfd
Brittany Hancock <femaleentp@gmail.com>
2025-10-12 16:45:41 -0700

    Week3: start MyStack with fields, constructor, and EnsureCapacityForOneMore; Push/Peek not implemented yet

---
+++ DataStructuresToolkit/StacksQueues/MyStack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace DataStructuresToolkit.StacksQueues
+{
+    /// <summary>
+    /// Generic stack with array-backed storage and doubling policy.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MyStack<T>
+    {
+        private T[] _items;
+        private int _count;
+        public MyStack(int initialCapacity = 4)
+        {
+            if (initialCapacity < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(initialCapacity));
+            }
+
+            _items = new T[initialCapacity];
+            _count = 0;
+        }
+
+        public int Count
+        {
+            get { return _count; }
+        }
+
+        // Complexity Time O(1) amortized and Space O(1) amortized
+        public void Push(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        // Complexity Time O(1) and Space O(1)
+        public T Peek()
+        { 
+            throw new NotImplementedException();
+        }
+
+        private void EnsureCapacityForOneMore()
+        {
+            if (_count < _items.Length)
+            {
+                return;
+            }
+
+            int newCapacity;
+            if (_items.Length == 0)
+            {
+                newCapacity = 4;
+            }
+            else
+            {
+                newCapacity = _items.Length * 2;
+            }
+
+            T[] newArray = new T[newCapacity];
+            for (int i = 0; i < _count; i++)
+            {
+                newArray[i] = _items[i];
+            }
+            _items = newArray;
+        }
+    }
+}
