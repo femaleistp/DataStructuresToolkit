053d79053c9d0fc60e0a44703a23059c8cde4646
Brittany Hancock <femaleentp@gmail.com>
2025-10-12 17:23:08 -0700

    Week3: start MyQueue with fields, constructor, and Count property.

--- DataStructuresToolkit/StacksQueues/MyQueue.cs
+++ DataStructuresToolkit/StacksQueues/MyQueue.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructuresToolkit.StacksQueues
 {
-    internal class MyQueue
+    /// <summary>
+    /// Generic queue with circular array storage and doubling policy.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class MyQueue<T>
     {
+        private T[] _items;
+        private int _count;
+        private int _head;
+        private int _tail;
+        public MyQueue(int intialCapacity = 4)
+        {
+            if (intialCapacity < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(intialCapacity));
+            }
+
+            _items = new T[intialCapacity];
+            _count = 0;
+            _head = 0;
+            _tail = 0;
+        }
+        // Complexity time O(1) and space O(n)
+        public int Count
+        {
+            get { return _count; }
+        }
     }
 }
