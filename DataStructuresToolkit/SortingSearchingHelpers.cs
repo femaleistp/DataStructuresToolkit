47be6d775b371516b0463936201287e55bdac807
Brittany Hancock <femaleentp@gmail.com>
2025-10-25 13:50:29 -0700

    Add SortingSearchingHelpers.cs with file header and class scaffold.  Add Demo_SortingSearching.cs for later possible performance view

---
+++ DataStructuresToolkit/SortingSearchingHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DataStructuresToolkit
+{
+    /// <summary>
+    /// Helper methods for sorting and searching algorithms.
+    /// </summary>
+    /// <remarks>Contains two searching algorithms (Linear, Binary) 
+    /// and two sorting algorithms (Bubble, Merge). Each method 
+    /// includes XML documentation for clarity.</remarks>
+    public static class SortingSearchingHelpers
+    {
+
+    }
+}
