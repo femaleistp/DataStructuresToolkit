d7d8713ab20d3bbda1699090e4439ec0099ce001
Brittany Hancock <femaleentp@gmail.com>
2025-10-25 14:02:47 -0700

    Add BinarySearch method

--- DataStructuresToolkit/SortingSearchingHelpers.cs
+++ DataStructuresToolkit/SortingSearchingHelpers.cs
@@ -125,5 +125,43 @@ namespace DataStructuresToolkit
             }
             return -1; // Target not found
         }
+
+        /// <summary>
+        /// Performs a binary search on a sorted array to find the index of the target value.
+        /// </summary>
+        /// <param name="arr">array of integers to search through (must be sorted)</param>
+        /// <param name="target">an integer value to search for</param>
+        /// <returns>a the index of the target if found; otherwise, -1</returns>
+        /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
+        /// <remarks>complexity time O(log n) and space O(1)</remarks>
+        public static int BinarySearch(int[] arr, int target)
+        {
+            if (arr == null)
+            { 
+                throw new ArgumentNullException(nameof(arr));
+            }
+
+            int left = 0;
+            int right = arr.Length - 1;
+
+            while(left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (arr[mid] == target)
+                {
+                    return mid; // Target found at index mid
+                }
+                else if (arr[mid] < target)
+                {
+                    left = mid + 1; // Search in the right half
+                }
+                else
+                {
+                    right = mid - 1; // Search in the left half
+                }
+            }
+            return -1; // Target not found
+        }
     }
 }
