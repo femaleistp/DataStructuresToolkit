4b97c015a794fe4730e00c5ce7e4e70483d774c0
Brittany Hancock <femaleentp@gmail.com>
2025-10-25 13:52:55 -0700

    Add BubbleSort method with XML documentation

--- DataStructuresToolkit/SortingSearchingHelpers.cs
+++ DataStructuresToolkit/SortingSearchingHelpers.cs
@@ -12,5 +12,33 @@ namespace DataStructuresToolkit
     public static class SortingSearchingHelpers
     {
 
+        /// <summary>
+        /// Sorts an array of integers in ascending order using the Bubble Sort algorithm.
+        /// </summary>
+        /// <param name="arr">The array of integers to be sorted.</param>
+        /// <exception cref="ArgumentNullException">Thrown when the input array is null.</exception>
+        /// <remarks>complexity time O(n^2) and space O(1)</remarks>
+        static void BubbleSort(int[] arr)
+        {
+            if (arr == null)
+            {
+                throw new ArgumentNullException(nameof(arr));
+            }
+
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // Swap arr[j] and arr[j+1]
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
