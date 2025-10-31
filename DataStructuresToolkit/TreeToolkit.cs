da4c4c3e7b5215c519478e2a3e9d59bc26a92369
Brittany Hancock <femaleentp@gmail.com>
2025-10-31 13:27:16 -0700

    Implement TreeNode class with BuildTeachingTree and three traversal methods

--- DataStructuresToolkit/TreeToolkit.cs
+++ DataStructuresToolkit/TreeToolkit.cs
@@ -7,6 +7,88 @@ namespace DataStructuresToolkit
     /// </summary>
     public class TreeToolkit
     {
+        
+    }
 
+    public class TreeNode
+    {
+        public int Value;
+        public TreeNode Left;
+        public TreeNode Right;
+
+        /// <summary>
+        /// Initializes a new instance of the TreeNode class with the specified value.
+        /// </summary>
+        /// <param name="value">The integer value of the node.</param>
+        /// <remarks>Complexity time O(1) and space O(1).</remarks>
+        public TreeNode(int value)
+        {
+            Value = value;
+            Left = null;
+            Right = null;
+        }
+
+        /// <summary>
+        /// Builds a sample binary tree for teaching purposes.
+        /// </summary>
+        /// <returns>The root node of the constructed binary tree.</returns>
+        /// <remarks>Complexity time O(1) and space O(1).</remarks>
+        public static TreeNode BuildTeachingTree()
+        { 
+            TreeNode root = new TreeNode(38);
+            root.Left = new TreeNode(27);
+            root.Right = new TreeNode(43);
+            root.Left.Left = new TreeNode(13);
+            root.Left.Right = new TreeNode(9);
+            return root;
+        }
+
+        /// <summary>
+        /// Performs an inorder traversal of the binary tree (left, root, right) and prints the node values.
+        /// </summary>
+        /// <param name="node">The root node of the binary tree.</param>
+        /// <remarks>Complexity time O(n) and space O(h) where n is the number of nodes and h is the height of the tree.</remarks>
+        public static void Inorder(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Inorder(node.Left);
+            Console.WriteLine(node.Value + " ");
+            Inorder(node.Right);
+        }
+
+        /// <summary>
+        /// Performs a preorder traversal of the binary tree (root, left, right) and prints the node values.
+        /// </summary>
+        /// <param name="node">The root node of the binary tree.</param>
+        /// <remarks>Complexity time O(n) and space O(h) where n is the number of nodes and h is the height of the tree.</remarks>
+        public static void Preorder(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+            Console.WriteLine(node.Value + " ");
+            Preorder(node.Left);
+            Preorder(node.Right);
+        }
+
+        /// <summary>
+        /// Performs a postorder traversal of the binary tree (left, right, root) and prints the node values.
+        /// </summary>
+        /// <param name="node">The root node of the binary tree.</param>
+        /// <remarks>Complexity time O(n) and space O(h) where n is the number of nodes and h is the height of the tree.</remarks>
+        public static void Postorder(TreeNode node)
+        { 
+            if (node == null)
+            {
+                return;
+            }
+            Postorder(node.Left);
+            Postorder(node.Right);
+            Console.WriteLine(node.Value + " ");
+        }
     }
 }
