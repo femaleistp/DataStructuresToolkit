e86a354d498f2a5751fb67db9e50f647f02a54ce
Brittany Hancock <femaleentp@gmail.com>
2025-10-31 13:19:37 -0700

    Add TreeToolkit.cs file with base namespace and summary scaffold

---
+++ DataStructuresToolkit/TreeToolkit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructuresToolkit
+{
+    /// <summary>
+    /// Contains basic binary tree and BST implementation with traversal and height utilities.
+    /// </summary>
+    public class TreeToolkit
+    {
+
+    }
+}
