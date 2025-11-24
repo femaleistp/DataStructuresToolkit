3060c8079f657bb5ad4cdedc7847455680781d46
Brittany Hancock <femaleentp@gmail.com>
2025-11-23 16:31:08 -0800

    feat: add initial SinglyLinkedList method stubs for TDD

--- DataStructuresToolkit/LinkedListHelpers.cs
+++ DataStructuresToolkit/LinkedListHelpers.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataStructuresToolkit
 {
-    internal class LinkedListHelpers
+    public class Node<T>
     {
+        public T Data;
+        public Node<T> Next;
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class SinglyLinkedList<T>
+    { 
+        public Node<T> Head { get; private set; }
+
+        public void AddFirst(T value)
+        {
+            var newNode = new Node<T>(value);
+            newNode.Next = Head;
+            Head = newNode;
+        }
+
+        // stubs for tdd
+
+        public void AddLast(T value)
+        {
+            // TODO: implement AddLast method
+            throw new NotImplementedException();
+        }
+
+        public T[] Traverse()
+        {
+            // TODO: implement Traverse method
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T value)
+        {
+            // TODO: implement Contains method
+            throw new NotImplementedException(); 
+        }
     }
 }
