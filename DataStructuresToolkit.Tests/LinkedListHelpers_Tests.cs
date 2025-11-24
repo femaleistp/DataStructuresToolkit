a713f437119441853537c898d11c3fb32da6786f
Brittany Hancock <femaleentp@gmail.com>
2025-11-23 16:16:38 -0800

    Add failing test for SinglyLinkedList AddFirst

---
+++ DataStructuresToolkit.Tests/LinkedListHelpers_Tests.cs
@@ -0,0 +1,23 @@
+﻿using DataStructuresToolkit;
+using Xunit;
+
+namespace DataStructuresToolkit.Tests
+{
+    public class SinglyLinkedListTests
+    {
+        [Fact]
+        public void AddFirst_ShouldInsertAtHead()
+        {
+            // Arrange
+            var list = new SinglyLinkedList<int>();
+
+            // Act
+            list.AddFirst(10);
+            list.AddFirst(20);
+
+            // Assert
+            Assert.Equal(20, list.Head.Data);
+            Assert.Equal(10, list.Head.Next.Data);
+        }
+    }
+}
