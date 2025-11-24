74dce44632c68053ff4978f3e2b9609acbb0671e
Brittany Hancock <femaleentp@gmail.com>
2025-11-23 16:17:47 -0800

    Add failing test for SinglyLinkedList Traverse

--- DataStructuresToolkit.Tests/LinkedListHelpers_Tests.cs
+++ DataStructuresToolkit.Tests/LinkedListHelpers_Tests.cs
@@ -19,5 +19,21 @@ namespace DataStructuresToolkit.Tests
             Assert.Equal(20, list.Head.Data);
             Assert.Equal(10, list.Head.Next.Data);
         }
+
+        [Fact]
+        public void Traverse_ShouldReturnSequence()
+        {
+            // Arrange
+            var list = new SinglyLinkedList<int>();
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast(3);
+
+            // Act  
+            var values = list.Traverse();
+
+            // Assert
+            Assert.Equal(new[] { 1, 2, 3 }, values);
+        }
     }
 }
