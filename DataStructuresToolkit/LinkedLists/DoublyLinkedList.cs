4ca848330cb9413a6a842373093d0da08be33118
Brittany Hancock <femaleentp@gmail.com>
2025-11-23 17:36:38 -0800

    Implement AddFirst in DoublyLinkedList (TDD: AddFirst test passing)

--- DataStructuresToolkit/LinkedLists/DoublyLinkedList.cs
+++ DataStructuresToolkit/LinkedLists/DoublyLinkedList.cs
@@ -11,10 +11,24 @@ namespace DataStructuresToolkit.LinkedLists
         public DoublyNode<T> Head { get; private set; }
         public DoublyNode<T> Tail { get; private set; }
 
-        // stub
+        /// <summary>
+        /// Adds a new node with the specified value at the beginning of the list.
+        /// </summary>
+        /// <param name="value">The value to add.</param>
+        /// <remarks>Complexity time O(1) and space O(1)</remarks>
         public void AddFirst(T value)
         {
-            // Implementation goes here
+            var newNode = new DoublyNode<T>(value);
+
+            if (Head == null)
+            {
+                Head = Tail = newNode;
+                return;
+            }
+
+            newNode.Next = Head;
+            Head.Prev = newNode;
+            Head = newNode;
         }
 
         public void AddLast(T value)
