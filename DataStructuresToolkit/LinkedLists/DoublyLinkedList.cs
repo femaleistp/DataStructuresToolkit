e3e0090b2044f22476c0990f93b4cd91485594b2
Brittany Hancock <femaleentp@gmail.com>
2025-11-23 17:42:32 -0800

    Implement TraverseBackward — test passing

--- DataStructuresToolkit/LinkedLists/DoublyLinkedList.cs
+++ DataStructuresToolkit/LinkedLists/DoublyLinkedList.cs
@@ -1,6 +1,7 @@
 ﻿using DataStructuresToolkit.LinkedLists.Nodes;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
 using System.Security.Policy;
 
 
@@ -31,6 +32,11 @@ namespace DataStructuresToolkit.LinkedLists
             Head = newNode;
         }
 
+        /// <summary>
+        /// Adds a new node with the specified value at the end of the list.
+        /// </summary>
+        /// <param name="value">The value to add.</param>
+        /// <remarks>Complexity time O(1) and space O(1)</remarks>
         public void AddLast(T value)
         {
             var newNode = new DoublyNode<T>(value);
@@ -60,8 +66,18 @@ namespace DataStructuresToolkit.LinkedLists
 
         public T[] TraverseBackward()
         {
-            // Implementation goes here
-            return Array.Empty<T>();
+            var result = new List<T>();
+            var current = Tail;
+
+            while (current != null)
+            {
+                result.Add(current.Data);
+                current = current.Prev;
+            }
+
+            return result.ToArray();
         }
+
+        
     }
 }
